0833d9406e16924aa2fb75677f563e26a970baf1
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-11 01:10:25 +0900

    feat : create Signup page

---
+++ src/pages/SignupPage.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Navigationbar from './components/Navigationbar';
+import Template from './components/Template';
+
+
+function SignupPage() {
+  return (
+    <div className="container">
+      <Navigationbar></Navigationbar>
+      <Template status="signup">
+      </Template>
+    </div>
+  );
+}
+
+export default SignupPage;
