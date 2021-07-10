9bb1393c1136b7b4e223575ab34fc107f76a8781
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-11 01:18:06 +0900

    chore : minor change(파일경로)

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 import React from 'react';
 import Navigationbar from './components/Navigationbar';
 import Template from './components/Template';
+import { BrowserRouter as Router } from 'react-router-dom';
+import SigninPage from './pages/SigninPage';
+import SignupPage from './pages/SignupPage';
+import MainPage from './pages/MainPage';
 
 
 function App() {
   return (
-    <div className="container">
-      <Navigationbar></Navigationbar>
-      <Template>
-      </Template>
-    </div>
+    <Router>
+      <MainPage></MainPage>
+      <SigninPage></SigninPage>
+      <SignupPage></SignupPage>
+    </Router>
   );
 }
 
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,6 +1,6 @@
-import './App.css';
+import '../App.css';
 import React from 'react';
-import Navigationbar from './components/Navigationbar';
+import Navigationbar from '../components/Navigationbar';
 
 
 function MainPage() {
