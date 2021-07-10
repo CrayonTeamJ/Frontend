aac9774e6651e5f19c2bae432fafeded1f031859
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-11 01:07:43 +0900

    feat : create mainPage

---
+++ src/pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import Navigationbar from './components/Navigationbar';
+
+
+function MainPage() {
+  return (
+    <div className="container">
+      <Navigationbar></Navigationbar>
+      <h1>THIS IS MAIN PAGE</h1>
+    </div>
+  );
+}
+
+export default MainPage;
