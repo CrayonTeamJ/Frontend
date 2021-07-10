cf86d4d9286cb0656792c3ce6905a486848c3196
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-11 01:09:57 +0900

    feat : create signin Page

---
+++ src/pages/SigninPage.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import Navigationbar from './components/Navigationbar';
+import Template from './components/Template';
+
+
+function SigninPage() {
+  return (
+    <div className="container">
+      <Navigationbar></Navigationbar>
+      <Template status="signin">
+      </Template>
+    </div>
+  );
+}
+
+export default SigninPage;
