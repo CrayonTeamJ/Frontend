4d0bbb0750b23ffa1ac3e681d69d0c05bd940254
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-11 01:57:08 +0900

    chore : delete not using style

--- src/components/Navigationbar.jsx
+++ src/components/Navigationbar.jsx
@@ -4,31 +4,6 @@ import styled from "styled-components";
 import { Link } from 'react-router-dom';
 
 
-const LoginButton = styled.div`
-  width: 80px;
-  height: 30px;
-  position: relative;
-  margin-left: 10px;
-  display: block;
-  text-align: center;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: rgba( 255, 255, 255, 0.0 );
-  border: 1px solid #85BCBE;
-`;
-
-const ResButton = styled.div`
-  width: 80px;
-  height: 30px;
-  position: relative;
-  margin-left: 10px;
-  display: block;
-  text-align: center;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: #85BCBE;
-  box-shadow: 0px 2px 3px 0px rgba(0, 0, 0, 0.4); /* 그림자효과 */
-`;
 
 
 const StyleSpan = styled.span`
