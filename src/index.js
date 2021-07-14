7e8767a442b7fd64c65bbc345946afed735189d4
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-15 02:44:24 +0900

    feat : (redux) store, rootreducer setting

--- src/index.js
+++ src/index.js
@@ -4,19 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import axios from "axios";
+import rootReducer from './redux';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+//import * as serviceWorker from './serviceWorker';
+
 
 //refresh Token cookie 을 주고받기 위한 설정 
 axios.defaults.withCredentials = true;
 
+const store = createStore(rootReducer, composeWithDevTools()); //스토어 생성 : 최상위 컴포넌트(app.js)가 랜더링 되는 곳(index.js) -> why? 하위 컴포넌트들이 모두 여기에 접근가능해야함
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+
 reportWebVitals();
+//serviceWorker.unregister();
---
+++ src/redux/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+//만들어낼 state의 action & reducer import 
+
+
+const rootReducer = combineReducers({
+  //추가하기 
+});
+
+export default rootReducer;
