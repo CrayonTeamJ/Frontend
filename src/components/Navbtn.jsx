1068508b8696573e6c48f7967e21b5d7478da96c
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-15 19:08:13 +0900

    feat : navbar 버튼 component생성

--- src/components/Navbtn.jsx
+++ src/components/Navbtn.jsx
@@ -1,12 +1,24 @@
 import '../App.css';
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
-function Navbtn() {
-  return (
-    <div>
-      <h1>new component btn</h1>
-    </div>
-  );
-}
+// 로그인, 로그아웃, 회원가입 버튼
+const Navbtn = (props) => (
+  <Link to={props.btnlink} className={props.className}>
+    <StyleSpan style={{ color: props.color }}>{props.btntype}</StyleSpan>
+  </Link>
+);
+
+const StyleSpan = styled.span`
+  position: absolute;
+  font-family: NanumSquare_R;
+  bottom: 30%;
+  font-size: 12px;
+  opacity: 1;
+  -webkit-transform: translateX(-50%);
+  transform: translateX(-50%);
+  white-space: nowrap;
+`;
 
 export default Navbtn;
