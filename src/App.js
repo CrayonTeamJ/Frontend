79914bef2ab00b57798ab80d14c471de02e4e3fa
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-17 02:49:41 +0900

    chore : code refactoring , 주석쓰기, 스타일 비율 약간수정 필요

--- src/App.js
+++ src/App.js
@@ -5,51 +5,14 @@ import './App.css';
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import axios from 'axios';
 import MainPage from './pages/MainPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
 import MyPage from './pages/MyPage';
-import { user_refresh } from './redux/users';
 
 function App() {
   const Key = useSelector((state) => state.users.access_token, []);
   const dispatch = useDispatch();
-  // const api = axios.create({
-  //   baseURL: 'http://localhost:5000'
-  // })
-
-  // const api = axios.create({
-  //   httpsAgent: new https.Agent({   //지금 밑에 콘솔창에 뜬 에러는 이거임 https없어서
-  //     rejectUnauthorized: false
-  //   }),
-  //   baseURL: 'https://localhost:5000'
-  // })
-  // api.get('https://localhost:5000/signup');
-
-  // useEffect(() => {
-  //   axios.get('http://localhost:5000/signup')      //endpoint. getRequest를 server 즉 index.js로 보내질 것
-  //   .then(response => console.log())   //server 에서 돌아온 response를 콘솔창에 출력해봄
-  // }, [])
-
-  useEffect(() => {
-    console.log('모든 페이지 리로드시 재발급 발생');
-    console.log('new key');
-    console.log(Key);
-    dispatch(user_refresh())
-      .then((res) => {
-        console.log('refresh성공');
-        if (res.payload.Result === 'success') {
-          // accesskey재 등록
-          axios.defaults.headers.common.Authorization = `Bearer ${Key}`;
-        } else {
-          console.log('refresh에 실패함');
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
 
   return (
     <>
