5587a524b2cd7f6a3fce277909da81b817f23aad
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-16 22:36:02 +0900

    chore: page전환시에도 navbar 정상인지 테스트하려고 만든 dummy page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MainPage from './pages/MainPage';
 import SignupPage from './pages/SignupPage';
 import SigninPage from './pages/SigninPage';
+import MyPage from './pages/MyPage';
 
 function App() {
   // const api = axios.create({
@@ -29,6 +30,7 @@ function App() {
         <Switch>
           <Route path="/login" component={SigninPage} />
           <Route path="/signup" component={SignupPage} />
+          <Route path="/profile" component={MyPage} />
           <Route path="/" component={MainPage} />
           {/** 이거 홈페이지가 위쪽에 있으면 안먹음 ...; */}
         </Switch>
---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+import '../App.css';
+import React from 'react';
+import Navigationbar from '../components/Navigationbar';
+import Footer from '../components/Footer';
+import LandingInfo from '../components/LandingInfo';
+
+function MyPage() {
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <div style={{ height: '6vh' }}>
+        <Navigationbar />
+      </div>
+      <div style={{ height: '89vh' }}>
+        {/* top container + bottom container */}
+        <div
+          className="top-container"
+          style={{ backgroundColor: 'red', height: '51vh' }}
+        >
+          <span>MYPAGE</span>
+        </div>
+        <div style={{ backgroundColor: 'blue', height: '38vh' }}>
+          <span>bottom continer</span>
+          <div className="bottom-wrapper">
+            <span>
+              button 3개가 들어가야 하는데 얘는 아마 라디오버튼이므로 일단은
+              보류
+            </span>
+          </div>
+        </div>
+      </div>
+      <div style={{ height: '5vh' }}>
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default MyPage;
