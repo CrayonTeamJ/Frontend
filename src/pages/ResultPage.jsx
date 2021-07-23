6534e1d569b2e8aa82bbc039d509d795ab0ec546
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-24 00:24:05 +0900

    feat : (result)미완기능 -> mainpage에서 요청보낸 검색결과 반환받을 시에 resultpage로 내용보내기

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -3,26 +3,37 @@
 import '../App.css';
 import React from 'react';
 import queryString from 'query-string';
+import { useLocation } from 'react-router';
 
-function ResultPage({ location }) {
-  const query = queryString.parse(location.search);
-  const video_id = query.id;
-  const type = query.searchtype;
-  const search_aud = query.searchaud;
-  const search_img = query.searchimg;
+function ResultPage() {
+  const location = useLocation();
 
-  //   const location = useLocation();
-
+  //   const query = queryString.parse(location.search);
+  //   const video_id = query.id;
+  //   const type = query.searchtype;
+  //   const search_aud = query.searchaud;
+  //   const search_img = query.searchimg;
+  const { res } = location.state;
+  //   console.log('result page');
+  //   console.log(res);
   return (
     <>
       <div className="top-container">
-        <span>{video_id}</span>
+        <span>
+          <div>
+            <span>{res.id}</span>
+            <span>{res.searchtype}</span>
+            <span>{res.searchimg}</span>
+            <span>{res.searchaud}</span>
+          </div>
+        </span>
+        {/* <span>{video_id}</span>
         <br />
         <span>{type}</span>
         <br />
         <span>{search_aud}</span>
         <br />
-        <span>{search_img}</span>
+        <span>{search_img}</span> */}
         <br />
       </div>
       <div className="bottom-container">
