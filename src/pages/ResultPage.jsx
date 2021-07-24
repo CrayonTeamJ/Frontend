ffbb54a64489de9a4f129b31d188dffd0e6c6a9f
YujeongLEE <dldbwjd24@ewhain.net>
2021-07-25 03:08:35 +0900

    feat : s3_url 이용한 player초안, layout잡는중

--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 /* eslint-disable camelcase */
 /* eslint-disable no-unused-vars */
 import '../App.css';
 import React from 'react';
 import queryString from 'query-string';
 import { useLocation } from 'react-router';
+import ReactPlayer from 'react-player';
 
 function ResultPage() {
   const location = useLocation();
@@ -14,30 +16,55 @@ function ResultPage() {
   //   const search_aud = query.searchaud;
   //   const search_img = query.searchimg;
   const { res } = location.state;
+  const results = res.result;
+  const video_infos = res.video_info;
+
   //   console.log('result page');
   //   console.log(res);
   return (
     <>
-      <div className="top-container">
-        <span>
-          <div>
-            <span>{res.id}</span>
-            <span>{res.searchtype}</span>
-            <span>{res.searchimg}</span>
-            <span>{res.searchaud}</span>
-          </div>
-        </span>
-        {/* <span>{video_id}</span>
-        <br />
-        <span>{type}</span>
-        <br />
-        <span>{search_aud}</span>
-        <br />
-        <span>{search_img}</span> */}
-        <br />
+      <div className="video-container">
+        <div className="video-grid-item" style={{ paddingTop: '30px' }}>
+          <ReactPlayer
+            url="https://teamj-data.s3.ap-northeast-2.amazonaws.com/video/bts_colbert.mp4"
+            controls="true"
+            pip="true"
+            width="960px"
+            height="540px"
+          />
+        </div>
+        <div className="video-grid-item">
+          <label
+            style={{
+              width: '960px',
+              fontFamily: 'NanumSquare_R',
+              fontSize: '25px',
+            }}
+          >
+            {video_infos.title}
+          </label>
+        </div>
       </div>
-      <div className="bottom-container">
-        <span>bottom container</span>
+      {/* <div className="result-container">
+        <div className="header">head</div>
+        <div className="button">but</div>
+        <div className="results">res</div>
+        <div className="button">but</div>
+        <div className="footer">foot</div>
+      </div> */}
+      <div className="grid-container">
+        <div className="grid-item header">header</div>
+        <div className="grid-item aside">aside</div>
+        <div className="grid-item content">
+          <div className="content-item">100</div>
+          <div className="content-item">100</div>
+          <div className="content-item">100</div>
+          <div className="content-item">100</div>
+          <div className="content-item">100</div>
+          <div className="content-item">100</div>
+        </div>
+        <div className="grid-item aside2">aside</div>
+        <div className="grid-item footer">footer</div>
       </div>
     </>
   );
